0742991f6174c1716f691b5d3698d4293a894995
CVOvidiu <cvovidiu2002@gmail.com>
2023-09-18 14:47:48 +0300

    Dummy reviews

---
+++ Reviews Carousel/modules/reviews.js
@@ -0,0 +1,32 @@
+// Dummy reviews
+
+const reviews = [
+  {
+    name: "Richard",
+    pfp: "https://images.unsplash.com/photo-1608848461950-0fe51dfc41cb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHx8fA%3D%3D&w=1000&q=80",
+    title: "QA Engineer",
+    review: "Lorem ipsum dolor sit amet.",
+  },
+  {
+    name: "Alin",
+    pfp: "https://static01.nyt.com/images/2021/09/14/science/07CAT-STRIPES/07CAT-STRIPES-mediumSquareAt3X-v2.jpg",
+    title: "HR",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, dignissimos? Doloribus temporibus illo sed inventore repellat nam quia reprehenderit odio quisquam. Quibusdam doloribus distinctio iusto magnam? Ducimus porro doloremque labore saepe. In laborum possimus sapiente dignissimos accusantium at voluptas neque nesciunt. Suscipit, magni perferendis quos voluptatem optio excepturi itaque voluptates.",
+  },
+  {
+    name: "Steven",
+    pfp: "https://images.pexels.com/photos/1170986/pexels-photo-1170986.jpeg?cs=srgb&dl=pexels-evg-kowalievska-1170986.jpg&fm=jpg",
+    title: "Automotive",
+    review: "Lorem, ipsum dolor.",
+  },
+  {
+    name: "Miguel",
+    pfp: "https://media.wired.co.uk/photos/60c8730fa81eb7f50b44037e/16:9/w_2560%2Cc_limit/1521-WIRED-Cat.jpeg",
+    title: "Software Engineer",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, quos mollitia.",
+  },
+];
+
+export default reviews;
