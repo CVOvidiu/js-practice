11b47e4f7530d35ae73900d3cfacfa19a9c00a0a
CVOvidiu <cvovidiu2002@gmail.com>
2024-05-03 13:03:39 +0300

    Menu: Bug fix

--- Menu/script.js
+++ Menu/script.js
@@ -5,6 +5,7 @@ const filters = document.querySelector(".filters");
 filter();
 
 filters.addEventListener("click", (e) => {
+  if (e.target.tagName == "LI") return 0;
   if (e.target.id == "all") filter();
   else filter(e.target.id);
 });
